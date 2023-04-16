f25e5b63a00a8be84e07f335ae9143ba5b656da3
vishnugprasad <vishnuanugraha001@gmail.com>
2023-04-16 13:53:47 +0530

    done

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,36 +1,34 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import styles from './Header.module.scss';
-import { useAppContext } from '../../appContext';
-
-
+import styles from "./Header.module.scss";
+import { useAppContext } from "../../appContext";
 
 function Header() {
-  const appContext = useAppContext()
-  console.log(appContext);
+  const appContext = useAppContext();
+
   return (
-    <Box sx={ styles.container }>
-    <AppBar position="static">
-      <Toolbar>
-      
-        <Typography variant="h6" component="div" className={styles.header}>
-          META CART 
-        </Typography>
-        
+    <Box sx={styles.container}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" className={styles.header}>
+            META CART
+          </Typography>
+
           <ShoppingCartIcon />
-          <Typography component="div" className={styles.name}>{appContext.name}</Typography>
-       
-      </Toolbar>
-    </AppBar>
-  </Box>
-  )
+          <Typography component="div" className={styles.name}>
+            {appContext.name}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
-export default Header
+export default Header;
--- pages/ProductList.js
+++ pages/ProductList.js
@@ -3,33 +3,31 @@ import axios from "axios";
 import ProductCard from "../components/ProductCard/ProductCard";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import styles from './ProductList.module.scss';
-
+import styles from "./ProductList.module.scss";
 
 function ProductList() {
   const [productList, setProductList] = useState([]);
   useEffect(() => {
     axios.get("https://fakestoreapi.com/products").then((response) => {
-      console.log(response.data);
       setProductList(response.data);
     });
   }, []);
   return (
     <Box sx={styles.container}>
-        <Grid container spacing = {2} >
-      {productList.map((elem) => {
-        return (
+      <Grid container spacing={2}>
+        {productList.map((elem) => {
+          return (
             <Grid item xs={4}>
-          <ProductCard key = {elem.id}
-            title={elem.title}
-            image={elem.image}
-            price={elem.price}
-            id = {elem.id}
-          ></ProductCard>
-        </Grid>
-          
-        );
-      })}
+              <ProductCard
+                key={elem.id}
+                title={elem.title}
+                image={elem.image}
+                price={elem.price}
+                id={elem.id}
+              ></ProductCard>
+            </Grid>
+          );
+        })}
       </Grid>
     </Box>
   );
